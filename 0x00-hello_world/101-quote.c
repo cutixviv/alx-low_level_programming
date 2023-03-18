69628775cd08b4d41d8c4ac4de43f8c8ba6ff29d
cutixviv <vivanuzoechi@gmail.com>
2023-03-18 01:48:01 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard erro
- * Return: 1 (Successful)
+ * Return: 1 (Success)
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
+}
